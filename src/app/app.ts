783a5edb253ade6da92bfcad6d394d6d4fadbe97
Krystian Chmielak <S00271020@atu.ie>
2026-05-05 23:44:53 +0200

    Added feedback message when added/ removed from diary

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
 import { Navbar } from './navbar/navbar';
+import { Feedback } from './feedback/feedback';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Feedback],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
