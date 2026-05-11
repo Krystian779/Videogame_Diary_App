755277b38345b53a5d4813893b40ba5dbe50c2eb
Krystian779 <krischmielak@gmail.com>
2026-05-12 00:16:05 +0100

    more debugging

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
-// import the libraries we installed with npm
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// create the Express app
 const app = express();
 
 console.log('Starting backend server...');
 
-// allow Angular frontend to call this backend API
 const corsOptions = {
   origin: 'https://main.d1a98ijehi68oz.amplifyapp.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -17,11 +14,14 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
-// allow server to understand JSON data sent in requests
 app.use(express.json());
 
-// define the structure of a diary game document in MongoDB
+// Remove API Gateway stage prefix if it is forwarded to Express
+app.use('/prod', (req, res, next) => {
+  req.url = req.url.replace(/^\/prod/, '');
+  next();
+});
+
 const diaryGameSchema = new mongoose.Schema({
   rawgId: Number,
   name: String,
@@ -32,16 +32,13 @@ const diaryGameSchema = new mongoose.Schema({
   userRating: Number,
 });
 
-// create a model based on the schema
 const DiaryGame = mongoose.model('DiaryGame', diaryGameSchema);
 
-// test route to prove the API is running
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
-// GET all diary games from MongoDB
-app.get(['/diary', '/prod/diary'], async (req, res) => {
+app.get('/diary', async (req, res) => {
   try {
     const games = await DiaryGame.find();
     res.status(200).json(games);
@@ -51,8 +48,7 @@ app.get(['/diary', '/prod/diary'], async (req, res) => {
   }
 });
 
-// POST a new diary game into MongoDB
-app.post(['/diary', '/prod/diary'], async (req, res) => {
+app.post('/diary', async (req, res) => {
   try {
     const game = new DiaryGame(req.body);
     const savedGame = await game.save();
@@ -64,8 +60,7 @@ app.post(['/diary', '/prod/diary'], async (req, res) => {
   }
 });
 
-// DELETE one diary game using its MongoDB _id
-app.delete(['/diary/:id', '/prod/diary/:id'], async (req, res) => {
+app.delete('/diary/:id', async (req, res) => {
   try {
     const deletedGame = await DiaryGame.findByIdAndDelete(req.params.id);
 
@@ -80,10 +75,8 @@ app.delete(['/diary/:id', '/prod/diary/:id'], async (req, res) => {
   }
 });
 
-// use port from .env, otherwise default to 5050
 const PORT = process.env.PORT || 5050;
 
-// connect to MongoDB first, then start server
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
